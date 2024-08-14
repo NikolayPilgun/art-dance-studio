96c026058de5e42e2dada1f834c50f2053b6a6a8
Nikolay Pilgun <a03198814s@gmail.com>
2024-08-14 14:23:39 +0500

    ContactElements

--- src/components/Contact/BannerContact/BannerContact.tsx
+++ src/components/Contact/BannerContact/BannerContact.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from "./BannerContact.module.scss";
 
-const BannerContact: React.FC = () => (
-	<div className={styles.container}>
-		<h2>наши контакты</h2>
-	</div>
-);
+const BannerContact: React.FC = () => <div className={styles.container}></div>;
 
 export default BannerContact;
--- src/components/Contact/Elements/ContactElements.tsx
+++ src/components/Contact/Elements/ContactElements.tsx
@@ -1,9 +1,50 @@
 import React from "react";
+import IconFour from "../../Icons/IconFour";
+import IconOne from "../../Icons/IconOne";
+import IconThree from "../../Icons/IconThree";
+import IconTwo from "../../Icons/IconTwo";
 import styles from "./ContactElements.module.scss";
 
 const ContactElements: React.FC = () => (
 	<div className={styles.container}>
-		<h2>ContactElements</h2>
+		<h2 className={styles.header}>Наши контакты</h2>
+
+		<div className={styles.innerContainer}>
+			<address className={styles.address}>
+				<p>
+					<strong>Адрес:</strong> г. Москва, Проспект Мира 68 ст2
+				</p>
+				<p>
+					<strong>Телефон:</strong> +7 495 120-18-19
+				</p>
+				<p>
+					<strong>Email:</strong> Icon-dance@yandex.ru
+				</p>
+				<p>
+					<strong>Часы работы:</strong> с 9:00 - 21:00 (без выходных)
+				</p>
+			</address>
+
+			<div className={styles.social}>
+				<span>
+					<IconOne
+						width={50}
+						height={50}
+						fillColor="black"
+						strokeColor="black"
+					/>
+				</span>
+				<span>
+					<IconTwo width={50} height={50} fillColor="black" />
+				</span>
+				<span>
+					<IconThree width={55} height={55} fillColor="black" />
+				</span>
+				<span>
+					<IconFour width={50} height={50} fillColor="black" />
+				</span>
+			</div>
+		</div>
 	</div>
 );
 
